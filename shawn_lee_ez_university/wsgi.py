9f58b755f4154d6ab925ce0a980540cd5ea6b7b2
Hsufeng Lee <thittalee@gmail.com>
2019-02-09 23:29:54 -0600

    init project

---
+++ shawn_lee_ez_university/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shawn_lee_ez_university project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shawn_lee_ez_university.settings')
+
+application = get_wsgi_application()
