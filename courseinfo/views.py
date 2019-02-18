ec119017939b13b120948e3756becb14e1d16183
Hsufeng Lee <thittalee@gmail.com>
2019-02-17 18:29:43 -0600

    Week 5 content: add templates

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Instructor, Section, Course, Semester, Student, Registration
+
+
+def instructor_list_view(request):
+    instructor_list = Instructor.objects.all()
+    # instructor_list = Instructor.objects.none()
+    template = loader.get_template("courseinfo/instructor_list.html")
+    context = {"instructor_list": instructor_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+
+def section_list_view(request):
+    section_list = Section.objects.all()
+    # section_list = Section.objects.none()
+    template = loader.get_template("courseinfo/section_list.html")
+    context = {"section_list": section_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+
+def course_list_view(request):
+    course_list = Course.objects.all()
+    # course_list = Course.objects.none()
+    template = loader.get_template("courseinfo/course_list.html")
+    context = {"course_list": course_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+
+def semester_list_view(request):
+    semester_list = Semester.objects.all()
+    # semester_list = Semester.objects.none()
+    template = loader.get_template("courseinfo/semester_list.html")
+    context = {"semester_list": semester_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+
+def student_list_view(request):
+    student_list = Student.objects.all()
+    # student_list = Student.objects.none()
+    template = loader.get_template("courseinfo/student_list.html")
+    context = {"student_list": student_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+
+def registration_list_view(request):
+    registration_list = Registration.objects.all()
+    # registration_list = Registration.objects.none()
+    template = loader.get_template("courseinfo/registration_list.html")
+    context = {"registration_list": registration_list}
+    output = template.render(context)
+    return HttpResponse(output)
--- shawn_lee_ez_university/urls.py
+++ shawn_lee_ez_university/urls.py
@@ -1,25 +1,15 @@
-"""shawn_lee_ez_university URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from courseinfo.views import instructor_list_view, section_list_view, course_list_view, semester_list_view, \
+    student_list_view, registration_list_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-
-
-
+    path('instructor/', instructor_list_view),
+    path('section/', section_list_view),
+    path('course/', course_list_view),
+    path('semester/', semester_list_view),
+    path('student/', student_list_view),
+    path('registration/', registration_list_view),
 ]
